84384a8e2ebba1ce23921378267531faa881e9e2
hoaingodong1 <hoaingodong1@gmail.com>
2023-02-21 22:47:03 +0700

    fix update brand profile

--- app/Http/Controllers/BrandProfileController.php
+++ app/Http/Controllers/BrandProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\BrandProfileRequest;
 use App\Helpers\FileHelper;
+use App\Models\Account;
 use App\Models\Credential;
 use App\Traits\ApiResponse;
 use Illuminate\Support\Facades\DB;
@@ -35,15 +36,38 @@ class BrandProfileController extends Controller
         $brandImage->save();
         return $this->responseSuccess();
     }
+    public function update(BrandProfileRequest $request,$brandId)
+    {
+        $credential = Credential::where('account_id',$brandId)->first();
+        if (empty($credential)) {
+            return $this->responseError('Brand does not exist!');
+        }
+        $credential->update
+        ([
+                'gender' => $request->gender,
+                'phone_number' => $request->phone_number,
+                'address_line1' => $request->address_line1,
+                'address_line2' => $request->address_line2,
+                'address_line3' => $request->address_line3,
+                'address_line4' => $request->address_line4,
+                'nickname' => $request->brand_name,
+                'dob' => $request->dob,
+                'industry' => $request->industry,
+                'website' => $request->website,
+                'fullname' => $request->fullname,             
+            ]);
+        // $credential->save();
+        $file = Account::with('files')->where('id', $brandId)->first();
+        FileHelper::removeFileFromS3($file);
+        $file->files()->delete();
+        $brandImage = FileHelper::uploadFileToS3($request->image, 'brands');
+        $brandImage->account_id = $credential->account_id;
+        $brandImage->save();
+        return $this->responseSuccess();
+    }
     public function view($account_id)
     {
-        $credential = DB::table('accounts')->join('credentials', 'accounts.id', '=', 'credentials.account_id')->join('files', 'files.id', '=', 'credentials.file_id')->where('account_id', $account_id)->get()->first();
+        $credential = DB::table('accounts')->join('credentials', 'accounts.id', '=', 'credentials.account_id')->join('files', 'credentials.id', '=', 'files.credential_id')->where('account_id', $account_id)->get()->first();
         return $this->responseSuccessWithData($credential->toArray());
-    }
-    public function viewAccount($account_id)
-    { {
-            $account = DB::table('accounts')->where('id', $account_id)->get();
-            return $this->responseSuccessWithData($account->toArray());
-        }
     }
 }
