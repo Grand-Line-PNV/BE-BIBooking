afaceb0c86110d101dd59808e0191e05f787c95b
thanhtu <ntu34543@gmail.com>
2023-03-20 14:49:36 +0700

    dashboard

--- resources/views/admin/headerMenu/index.blade.php
+++ resources/views/admin/headerMenu/index.blade.php
@@ -7,11 +7,11 @@
 <!-- partial:partials/_navbar.html -->
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo" href="/admin/overview"><img
+        <a class="navbar-brand brand-logo" href="/admin/dashboard"><img
                 src="/assets/images/LogoAdmin.png" alt="logo" />
             <span class="content">&nbsp;</span>
         </a>
-        <a class="navbar-brand brand-logo-mini" href="/admin/overview"><img
+        <a class="navbar-brand brand-logo-mini" href="/admin/dashboard"><img
                 src="/assets/images/logoBIBooking.png" alt="logo" /></a>
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-stretch">
